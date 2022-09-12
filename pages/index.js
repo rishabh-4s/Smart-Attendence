d7d5473115c7421397d364642e67d73d6d702fe2
Arjit <arjitsingh0609@gmail.com>
2022-09-12 23:31:50 +0530

    First commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Smart Attendance </title>
+        <meta name="description" content="This is how we do it." />
+        <link rel="icon" href="/icons8-immigration-64.png" />
+      </Head>
+      This is how we do it so
+
+    </div>
+  );
+}
