c56baeef7e57ee5b860bc96f73064780de15fb80
Arjit <arjitsingh0609@gmail.com>
2022-10-29 00:59:41 +0530

    6th arjit

--- pages/index.js
+++ pages/index.js
@@ -9,22 +9,28 @@ export default function Home() {
         <meta name="description" content="This is how we do it." />
         <link rel="icon" href="/icons8-immigration-64.png" />
       </Head>
-      <div className="total__bg">
-        <div className="index__everything">
-          <div className="index__links">
-            <Link href="/attendance_table">
-              <button className="button1">Attendance Page</button>
-            </Link>
 
-            <Link href="/login">
-              <button className="button2">Teacher Login Page</button>
-            </Link>
-          </div>
+      <div className="total__bg">
+        
+        <div className="index__links">
+          <Link href="/attendance_table">
+            <button className="button1">Attendance Page</button>
+          </Link>
+
+          <Link href="/login">
+            <button className="button2">Teacher Login Page</button>
+          </Link>
         </div>
         <img
           src="http://smartattendanceapp.com/img/footerlogo.png"
           className="index__login"
         ></img>
+        
+        <div className="write_up">
+          IMPROVE THE WRITE UP. A revolutionary piece of software that
+          simplifies the tedious task of taking attendance hassle free. Please
+          go ahead and try this for yourself.
+        </div>
       </div>
     </>
   );
