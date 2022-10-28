c2dab902af3f5897856c2401df235fc9dbf0edae
Arjit <arjitsingh0609@gmail.com>
2022-10-29 01:15:28 +0530

    tanish

--- pages/checkattendance.js
+++ pages/checkattendance.js
@@ -1,7 +1,8 @@
 import { React, useState } from "react";
-import List from "./List"
-import TextField from "@mui/material/TextField"
-import styles from "../styles/checkattendance.module.css"
+import Head from "next/head";
+import List from "./List";
+import TextField from "@mui/material/TextField";
+import styles from "../styles/checkattendance.module.css";
 
 const checkattendance = () => {
   const [inputText, setInputText] = useState("");
@@ -11,20 +12,26 @@ const checkattendance = () => {
     setInputText(lowerCase);
   };
   return (
-    <div className={styles.main}>
-      <h1 className={styles.abc}>Student Search</h1>
-      <div className={styles.search}>
-      <TextField
-          id="outlined-basic"
-          onChange={inputHandler}
-          variant="outlined"
-          fullWidth
-          label="Search"
-        />
-      </div>
-      <List input={inputText} />
-    </div>
-  )
-}
+    <div>
+      <Head>
+        <title>Check Attendance</title>
+      </Head>
 
-export default checkattendance
+      <div className={styles.main}>
+        <h1 className={styles.abc}>Student Search</h1>
+        <div className={styles.search}>
+          <TextField
+            id="outlined-basic"
+            onChange={inputHandler}
+            variant="outlined"
+            fullWidth
+            label="Search"
+          />
+        </div>
+        <List input={inputText} />
+      </div>
+    </div>
+  );
+};
+
+export default checkattendance;
