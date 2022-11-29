1597f46de0f452b5e5a250ecf4fa3e77e1cbcd0c
tanish593 <77679407+tanish593@users.noreply.github.com>
2022-11-29 08:34:59 +0530

    12th

--- pages/checkattendance.js
+++ pages/checkattendance.js
@@ -4,20 +4,22 @@ import List from "./List";
 import TextField from "@mui/material/TextField";
 import styles from "../styles/checkattendance.module.css";
 
-const checkattendance = () => {
-  const [inputText, setInputText] = useState("");
-  let inputHandler = (e) => {
-    //convert input text to lower case
-    var lowerCase = e.target.value.toLowerCase();
-    setInputText(lowerCase);
-  };
+function checkattendance() {
   return (
     <div>
       <Head>
         <title>Check Attendance</title>
       </Head>
+      
+      <iframe
+        src="https://docs.google.com/spreadsheets/d/12hmNRarKNkN5KDoHeybMiNX8SehMR2Jy2pEe8Hv_NQM/edit?usp=sharing"
+        width="1550"
+        height="800"
+        className="iframe_table"
+        style={{ padding: 100 + "px" }}
+      />
 
-      <div className={styles.main}>
+      {/* <div className={styles.main}>
         <h1 className={styles.abc}>Student Search</h1>
         <div className={styles.search}>
           <TextField
@@ -29,9 +31,9 @@ const checkattendance = () => {
           />
         </div>
         <List input={inputText} />
-      </div>
+      </div> */}
     </div>
   );
-};
+}
 
 export default checkattendance;
